36baa33f5b6c9cadec935c78f12ee5f5d6b00c44
netrunner <NETRUNNER4@protonmail.com>
2022-01-14 04:05:41 +0200

    Works with all words in message and adds user's first name

--- main.py
+++ main.py
@@ -9,14 +9,19 @@ updater = Updater(token=token, use_context=True)
 dispatcher = updater.dispatcher
 
 
-def ilya(update: Update, context: CallbackContext):
-    wordlist = ['илья', 'ильи', 'илье', 'илью', 'ильей']
-    for word in wordlist:
-        if word in update.message.text.lower():
-            pos = update.message.text.lower().find(word)
-            realword = update.message.text[pos:pos+len(word)]
-            context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text.replace(realword, realword.replace('ь', '').replace('Ь', '')))
-            break
+def ilya(update: Update, context: CallbackContext) -> None:
+    tg_message = update.message.text
+    tg_user = update.message.from_user
+    wordlist = [' илья ', ' ильи ', ' илье ', ' илью ', ' ильей ']  # ё?. Не поломаются ли от пробелов позиции - на практике вроде нет, но нужно понять как код работает
+    if any(c in tg_message.lower() for c in wordlist):  # Этот if нужно убрать, при этом чтобы сообщение не отправлялось
+        while any(c in tg_message.lower() for c in wordlist):
+            for word in wordlist:
+                pos = tg_message.lower().find(word)
+                if pos != -1:  # оно и без этого работает не багаясь почему-то, но пусть будет
+                    realword = tg_message[pos:pos + len(word)]
+                    tg_message = tg_message.replace(realword, realword.replace('ь', '').replace('Ь', ''))
+        reply = str(tg_user['first_name'])+': '+tg_message  # работает без str, но предупреждение из-за какой-то хуйни в либе
+        context.bot.send_message(chat_id=update.effective_chat.id, text=reply)
 
 
 ilya_handler = MessageHandler(Filters.text & (~Filters.command), ilya)
