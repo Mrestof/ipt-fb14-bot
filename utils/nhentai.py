8339f6300106ed962eb94ff291800543545426a7
netrunner4 <netrunner4@protonmail.com>
2022-01-25 04:01:15 +0200

    Take first page of doujin instead of second

--- utils/nhentai.py
+++ utils/nhentai.py
@@ -5,7 +5,7 @@ from NHentai import NHentai
 def download_hentai():
     nhentai = NHentai()
     Doujin = nhentai.get_random()
-    image_info = Doujin.images[1].src
+    image_info = Doujin.images[0].src
     filename = str(Doujin.id)
     os.system(f'wget --limit-rate=1m -O {filename} {image_info}')  # TODO: check the OS beforehand
     return filename
