48606c9d58d19c546e6959db9c657b6052984027
netrunner4 <netrunner4@protonmail.com>
2022-05-27 16:40:25 +0200

    Add sticker handler

--- utils/init.py
+++ utils/init.py
@@ -3,7 +3,7 @@ from telegram.ext import Updater
 from telegram.ext import MessageHandler, Filters, CommandHandler
 
 from handlers.text import text_messages
-from handlers.commands import hentai, pasha_nick, auf, ero, ecchi, photo, minecraft, deadinside, auf_markov,\
+from handlers.command import hentai, pasha_nick, auf, ero, ecchi, photo, minecraft, deadinside, auf_markov,\
     pavelko_markov, razum_markov, khashcha_markov, bolgov_markov, semen_markov, frolov_markov, makuha_markov, \
     david_markov, edward_markov, oleg_markov
 from handlers.image import photo_messages
@@ -12,6 +12,7 @@ from handlers.audio import audio_messages
 from handlers.animation import animation_messages
 from handlers.video_note import video_note_messages
 from handlers.voice import voice_messages
+from handlers.sticker import sticker_messages
 
 # Function to get token from file
 
@@ -73,6 +74,9 @@ def get_updater(token: str) -> Updater:
 
     animation_handler = MessageHandler(Filters.animation & (~Filters.command), animation_messages)
     dispatcher.add_handler(animation_handler)
+
+    sticker_handler = MessageHandler(Filters.animation & (~Filters.command), sticker_messages)
+    dispatcher.add_handler(sticker_handler)
 
     pasha_nick_handler = CommandHandler('I3700ch3g0', pasha_nick)
     dispatcher.add_handler(pasha_nick_handler)
