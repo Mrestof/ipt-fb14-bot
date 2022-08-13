992a1ba6ed69d4e6c9dabcfd196ae0ef47fedb3b
Mrestof <fotserm@gmail.com>
2022-08-13 19:06:52 +0300

    wp: Add basic json logging

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+from utils.log import get_main_logger
 from utils.init import get_token, get_updater, set_commands
 
 
 def main():
+    logger = get_main_logger()
+
     token = get_token()  # Getting Bot Token from a file
 
     set_commands(token)  # Setting commands description
     updater = get_updater(token)  # Bot Updater (Function for initiation)
-    updater.start_polling()  # Bot Start Function
 
-    print('Start the bot. Logs go here.')
+    logger.info('Start the bot. Logs go here.')
+    updater.start_polling()  # Bot Start Function
     updater.idle()
 
 
---
+++ utils/log.py
@@ -0,0 +1,83 @@
+import logging
+
+# --------------------------------------------------------------------------
+#   Constants
+# --------------------------------------------------------------------------
+"""
+%(name)s            Name of the logger (logging channel)
+%(levelno)s         Numeric logging level for the message (DEBUG, INFO,
+                    WARNING, ERROR, CRITICAL)
+%(levelname)s       Text logging level for the message ("DEBUG", "INFO",
+                    "WARNING", "ERROR", "CRITICAL")
+%(pathname)s        Full pathname of the source file where the logging
+                    call was issued (if available)
+%(filename)s        Filename portion of pathname
+%(module)s          Module (name portion of filename)
+%(lineno)d          Source line number where the logging call was issued
+                    (if available)
+%(funcName)s        Function name
+%(created)f         Time when the LogRecord was created (time.time()
+                    return value)
+%(asctime)s         Textual time when the LogRecord was created
+%(msecs)d           Millisecond portion of the creation time
+%(relativeCreated)d Time in milliseconds when the LogRecord was created,
+                    relative to the time the logging module was loaded
+                    (typically at application startup time)
+%(thread)d          Thread ID (if available)
+%(threadName)s      Thread name (if available)
+%(process)d         Process ID (if available)
+%(message)s         The result of record.getMessage(), computed just as
+                    the record is emitted
+"""
+
+TOPLEVEL_LOGGER_NAME = 'main'
+JSON_LOG_FORMAT_ATTRS = (
+    'asctime', 'levelname',
+    'filename', 'funcName', 'lineno',
+    'message',
+)
+
+
+# TODO: refactor this cringe piece
+def _wrap(attr_name: str) -> str:
+    json_attrs_to_wrap = ("name", "levelno", "levelname", "pathname", "filename",
+                          "module", "funcName", "asctime", "threadName", "message")
+    if attr_name in json_attrs_to_wrap:
+        return '"{' + attr_name + '}"'
+    else:
+        return '{' + attr_name + '}'
+
+
+JSON_LOG_FORMAT = '{{' + ','.join(f'"{attr}":{_wrap(attr)}' for attr in JSON_LOG_FORMAT_ATTRS) + '}}'
+
+# ---------------------------------------------------------------------------
+#   Utilities
+# ---------------------------------------------------------------------------
+
+
+def get_logger_name(module_name: str) -> str:
+    """Append the correct parent logger to the logger name
+
+    :param module_name: name from `__name__` variable
+    :return:
+    """
+    return f'{TOPLEVEL_LOGGER_NAME}.{module_name}'
+
+
+def get_main_logger(
+        name: str = TOPLEVEL_LOGGER_NAME,
+        level: int = logging.DEBUG,
+        log_format: str = JSON_LOG_FORMAT,
+) -> logging.Logger:
+    formatter = logging.Formatter(fmt=log_format, style='{')
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(level)
+    stream_handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(stream_handler)
+
+    logger.info('Initialized the main logger')
+    return logger
