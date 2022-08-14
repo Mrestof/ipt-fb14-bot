52a0718722da1017e7b8c80376bae986353bf11d
Mrestof <fotserm@gmail.com>
2022-08-14 15:25:31 +0300

    Remove excess comment (whoopsie)

--- utils/log.py
+++ utils/log.py
@@ -3,33 +3,6 @@ import logging
 # --------------------------------------------------------------------------
 #   Constants
 # --------------------------------------------------------------------------
-"""
-%(name)s            Name of the logger (logging channel)
-%(levelno)s         Numeric logging level for the message (DEBUG, INFO,
-                    WARNING, ERROR, CRITICAL)
-%(levelname)s       Text logging level for the message ("DEBUG", "INFO",
-                    "WARNING", "ERROR", "CRITICAL")
-%(pathname)s        Full pathname of the source file where the logging
-                    call was issued (if available)
-%(filename)s        Filename portion of pathname
-%(module)s          Module (name portion of filename)
-%(lineno)d          Source line number where the logging call was issued
-                    (if available)
-%(funcName)s        Function name
-%(created)f         Time when the LogRecord was created (time.time()
-                    return value)
-%(asctime)s         Textual time when the LogRecord was created
-%(msecs)d           Millisecond portion of the creation time
-%(relativeCreated)d Time in milliseconds when the LogRecord was created,
-                    relative to the time the logging module was loaded
-                    (typically at application startup time)
-%(thread)d          Thread ID (if available)
-%(threadName)s      Thread name (if available)
-%(process)d         Process ID (if available)
-%(message)s         The result of record.getMessage(), computed just as
-                    the record is emitted
-"""
-
 TOPLEVEL_LOGGER_NAME = 'main'
 JSON_LOG_FORMAT_ATTRS = (
     'asctime', 'levelname',
@@ -48,11 +21,10 @@ def _wrap(attr_name: str) -> str:
 
 JSON_LOG_FORMAT = '{{' + ','.join(f'"{attr}":{_wrap(attr)}' for attr in JSON_LOG_FORMAT_ATTRS) + '}}'
 
+
 # ---------------------------------------------------------------------------
 #   Utilities
 # ---------------------------------------------------------------------------
-
-
 def get_logger_name(module_name: str) -> str:
     """Append the correct parent logger to the logger name
 
