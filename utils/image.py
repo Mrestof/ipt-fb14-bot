d7ef9ddf3bd4f24add86b9e51be3e3c523c3276d
netrunner4 <netrunner4@protonmail.com>
2023-05-05 21:55:08 +0200

    Refactor image.py

--- utils/image.py
+++ utils/image.py
@@ -1,4 +1,3 @@
-# TODO: explain magic numbers or refactor the blocks of code with them or refactor the whole module
 import random
 import requests
 import os
@@ -8,42 +7,50 @@ from bs4 import BeautifulSoup
 from NHentai import NHentai
 
 
-# TODO: rewrite
-def crawl(images_type):
+def crawl(images_type) -> str:
     url = ''
-    if images_type == 'photo':
-        url = 'https://wallhaven.cc/search?categories=100&purity=100&ratios=16x9&topRange=1y&sorting=toplist&order' \
-              f'=desc&page={random.randint(1, 100)} '
-    elif images_type == 'ero':
-        url = 'https://wallhaven.cc/search?categories=001&purity=010&topRange=1y&sorting=toplist&order=desc&page=' \
-              f'{random.randint(1, 300)}'
-    elif images_type == 'ecchi':
-        url = 'https://wallhaven.cc/search?categories=010&purity=010&topRange=1y&sorting=toplist&order=desc&page=' \
-              f'{random.randint(1, 300)}'
+    photo_page_range = (1, 100)
+    ero_page_range = (1, 300)
+    ecchi_page_range = (1, 300)
+    wallhaven_link = (
+        'https://wallhaven.cc/search'
+        '?categories={ctg}&purity={pur}&topRange=1y&sorting=toplist&order=desc&page={page}'
+    )
+
+    match images_type:
+        case 'photo':
+            url = wallhaven_link.format(ctg='100', pur='100', page=random.randint(*photo_page_range))
+        case 'ero':
+            url = wallhaven_link.format(ctg='001', pur='010', page=random.randint(*ero_page_range))
+        case 'ecchi':
+            url = wallhaven_link.format(ctg='010', pur='010', page=random.randint(*ecchi_page_range))
+
     r = requests.get(url)
     soup = BeautifulSoup(r.content, features="html.parser")
-    ss = soup.findAll('a', {'class': 'preview'})
-    filename_list = []
-    for s in ss:
-        filename = (str(s)[48:54])
-        filename_list.append(filename)
-    chosen_filename = random.choice(filename_list)
-    chosen_filename_part = chosen_filename[:2]
-    url = f'https://w.wallhaven.cc/full/{chosen_filename_part}/wallhaven-{chosen_filename}.jpg'
+    a_tags = soup.findAll('a', {'class': 'preview'})
+
+    file_id_list = []
+    file_id_tag_slice = slice(48, 54)
+    for a_tag in a_tags:
+        file_id = (str(a_tag)[file_id_tag_slice])
+        file_id_list.append(file_id)
+    chosen_file_id = random.choice(file_id_list)
+    chosen_file_id_part = chosen_file_id[:2]  # wallhaven creates subdirectories by first 2 letters of image's id
+    url = f'https://w.wallhaven.cc/full/{chosen_file_id_part}/wallhaven-{chosen_file_id}.jpg'
     return url
 
 
-# TODO: somehow save the useful info on fail of this function
-def url_check(url):
+def url_check(url) -> str:
     r = requests.get(url)
     if r.status_code != 200:
         url = url[:-3] + "png"
     return url
 
 
-def download_wallhaven(image_type):
+def download_wallhaven(image_type) -> str:
     image_url = url_check(crawl(image_type))
-    filename = image_url[31:47]
+    filename_slice = slice(31, 47)
+    filename = image_url[filename_slice]
     os.system(f'wget --limit-rate=2m -O {filename} {image_url}')
     return filename
 
@@ -54,42 +61,37 @@ def remove_file(path):
 
 
 def resize_image(path, max_size):
-    # TODO: cut indents
-    if os.path.isfile(path):
-        try:
-            im = Image.open(path)
-            if im.height > max_size or im.width > max_size:
-                f, e = os.path.splitext(path)
+    try:
+        im = Image.open(path)
+        if not im.height > max_size or not im.width > max_size:
+            return None
 
-                # if width > height:
-                if im.width > im.height:
-                    desired_width = max_size
-                    desired_height = im.height / (im.width / max_size)
+        f, e = os.path.splitext(path)
 
-                # if height > width:
-                elif im.height > im.width:
-                    desired_height = max_size
-                    desired_width = im.width / (im.height / max_size)
+        if im.width > im.height:
+            desired_width = max_size
+            desired_height = im.height / (im.width / max_size)
 
-                else:
-                    desired_height = max_size
-                    desired_width = max_size
+        elif im.height > im.width:
+            desired_height = max_size
+            desired_width = im.width / (im.height / max_size)
 
-                # convert back to integer
-                desired_height = int(desired_height)
-                desired_width = int(desired_width)
+        else:
+            desired_height = max_size
+            desired_width = max_size
 
-                im_resized = im.resize((desired_width, desired_height))
-                # im_resized.save(f + '_resized.jpg', 'JPEG', quality=90) # doesn't overwrite file
-                if im.format == 'JPEG':
-                    im_resized.save(f + e, 'JPEG', quality=80)  # does overwrite file
-                elif im.format == 'PNG':
-                    im_resized.save(f + e, 'PNG', quality=80)  # format
-                else:
-                    print(str(im.format) + ' Unknown format')
-                # im_resized.save('resized_' + f + '.jpg', 'JPEG', quality=100)
-        except Exception as e:
-            print(e)
+        desired_height = int(desired_height)
+        desired_width = int(desired_width)
+
+        im_resized = im.resize((desired_width, desired_height))
+        if im.format == 'JPEG':
+            im_resized.save(f + e, 'JPEG', quality=80)  # does overwrite file
+        elif im.format == 'PNG':
+            im_resized.save(f + e, 'PNG', quality=80)  # format
+        else:
+            print(str(im.format) + ' Unknown format')
+    except Exception as e:
+        print(e)
 
 
 def download_hentai():
